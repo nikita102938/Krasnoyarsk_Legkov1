dca7aaac4bcbc160d0079ddd377de9548d981745
nikita102938 <legkov07@inbox.ru>
2025-02-19 18:32:39 +0700

    :)

---
+++ main2.py
@@ -0,0 +1,49 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QPushButton
+from PyQt6.QtGui import QPainter, QColor, QPolygon
+import sys
+import random
+from UI import Ui_MainWindow
+
+SCREEN_SIZE = [680, 480]
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.fl = False
+        self.setWindowTitle('gg')
+        self.pushButton.clicked.connect(self.draw)
+        self.crd = []
+
+    def draw(self):
+        self.fg = 'circle'
+        self.size = random.randint(10, 100)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # 'yellow'
+        self.fl = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.fl:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
+            self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
+            if self.fg == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
+            qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
